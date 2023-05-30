2862e76f34caf90ae7a107e7815b3775555de3fc
Finn Groenewoud <finn.groenewoud@gmail.com>
2023-05-30 09:38:34 +0200

    expanded faker

--- Framework/Helpers/Faker.php
+++ Framework/Helpers/Faker.php
@@ -7,6 +7,7 @@ class Faker
 	private $prefixes;
 	private $firstnames;
 	private $lastnames;
+	private $emails;
 
 	public function __construct()
 	{
@@ -46,6 +47,18 @@ class Faker
 			'Janssen',
 			'Janssen',
 		];
+
+		$this->emails = [
+			'@gmail.com',
+			'@hotmail.com',
+			'@outlook.com',
+			'@example.com',
+			'@example.org',
+			'@yahoo.com',
+			'@live.com',
+			'@live.nl',
+			'@live.be',
+		];
 	}
 
 	public function name()
@@ -61,5 +74,64 @@ class Faker
 	public function lastname()
 	{
 		return $this->lastnames[array_rand($this->lastnames)];
+	}
+
+	public function number()
+	{
+		return rand(0, 2147483647);
+	}
+
+	public function numberBetween($min, $max)
+	{
+		return rand($min, $max);
+	}
+
+	public function email()
+	{
+		return $this->firstname() . '.' . $this->lastname() . $this->emails[array_rand($this->emails)];
+	}
+
+	public function password()
+	{
+		return password_hash('password', PASSWORD_DEFAULT);
+	}
+
+	public function date()
+	{
+		return date('d-m-Y', mktime(0, 0, 0, rand(1, 12), rand(1, 28), rand(1900, 2020)));
+	}
+
+	public function datetime()
+	{
+		return date('d-m-Y H:i:s', mktime(rand(0, 23), rand(0, 59), rand(0, 59), rand(1, 12), rand(1, 28), rand(1900, 2020)));
+	}
+
+	public function time()
+	{
+		return date('H:i:s', mktime(rand(0, 23), rand(0, 59), rand(0, 59), rand(1, 12), rand(1, 28), rand(1900, 2020)));
+	}
+
+	public function text($length = 255)
+	{
+		$text = '';
+		$possible = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 ';
+
+		for ($i = 0; $i < $length; $i++) {
+			$text .= $possible[rand(0, strlen($possible) - 1)];
+		}
+
+		return $text;
+	}
+
+	public function textBetween($min, $max)
+	{
+		$text = '';
+		$possible = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 ';
+
+		for ($i = 0; $i < rand($min, $max); $i++) {
+			$text .= $possible[rand(0, strlen($possible) - 1)];
+		}
+
+		return $text;
 	}
 }
